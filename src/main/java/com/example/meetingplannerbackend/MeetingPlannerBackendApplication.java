3a6ad3146db01026154251def3683d6d3d73ac02
wairimu ian <wairimuian@gmail.com>
2021-09-30 11:04:45 +0300

    this is meeting-planner-backend commit

---
+++ src/main/java/com/example/meetingplannerbackend/MeetingPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetingplannerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingPlannerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingPlannerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meetingplannerbackend/MeetingPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetingplannerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingPlannerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
